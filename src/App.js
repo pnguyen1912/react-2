ee9979495bc335582bdc317d99ce83bdbd7e89aa
Patton Nguyen <pattonnguyen@northwesternmutual.com>
2019-06-11 14:30:16 -0500

    finish function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Voting from './Voting'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    options: [
+      {
+        id: 1,
+        name: 'React',
+        count: 0
+      },
+      {
+        id: 2,
+        name: 'Angular',
+        count: 0
+      },
+      {
+        id: 3,
+        name: 'Vue',
+        count: 0
+      },
+      {
+        id: 4,
+        name: 'Ember',
+        count: 0
+      },
+      {
+        id: 5,
+        name: 'jQuery',
+        count: 0
+      },
+    ]
+  }
+
+  handleChange = (input, type) => {
+    if (type === 'add') {
+      this.setState({
+        options: this.state.options.map(item => {
+          if (item.name === input.name) {
+            return Object.assign({}, item, {
+              count: input.count + 1
+            })
+          } else {
+            return item;
+          }
+        }).sort(function (a, b) { return b.count - a.count })
+      })
+
+    } else if (type === 'minus') {
+      this.setState({
+        options: this.state.options.map(item => {
+          if (item.name === input.name) {
+            return Object.assign({}, item, {
+              count: input.count - 1
+            })
+          } else {
+            return item;
+          }
+        })
+      })
+    }
+
+
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>JavaScript Voting App</h1>
+
+        {this.state.options.map(item => {
+          return (
+            <div>
+              <h3>{item.count}
+                <Voting name={item.name} />
+                <button onClick={() => { this.handleChange(item, 'minus') }} >-</button>
+                <button onClick={() => { this.handleChange(item, 'add') }} >+</button></h3>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Voting.js
@@ -0,0 +1,17 @@
+import React from 'react'
+class Voting extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+
+
+
+}
+
+export default Voting;
